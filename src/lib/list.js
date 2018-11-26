d033b3b214f4a370a7f0242becfb733d9f8a153c
kristinngodfrey <kristinngodfrey@gmail.com>
2018-11-26 15:20:31 +0000

    push1

--- src/lib/list.js
+++ src/lib/list.js
@@ -1,11 +1,39 @@
 import { empty } from './helpers';
+import { generateImage, generateTitle } from './converter';
 
 export default class List {
   constructor() {
     this.container = document.querySelector('.list');
+    this.url = '../lectures.json';
+  }
+
+  loadLectures() {
+    return fetch(this.url)
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error('gat ekki sótt fyrirlestur');
+        }
+        return res.json();
+      });
+  }
+
+  renderData(data) {
+    data.lectures.map((item) => {
+      this.renderItem(item);
+    });
+  }
+
+  renderItem(item) {
+    const titleElement = generateTitle(item.title);
+    this.container.appendChild(titleElement);
+
+    const imageElement = generateImage(item.thumnail);
+    this.container.appendChild(imageElement);
   }
 
   load() {
     empty(this.container);
+    this.loadLectures()
+      .then((data) => this.renderData(data));
   }
 }
